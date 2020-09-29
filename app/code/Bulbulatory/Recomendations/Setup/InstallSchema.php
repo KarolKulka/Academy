0615fbf66aebb65ffab28e593c664e398139e44e
Kulka <siwkakarol@gmail.com>
2020-09-29 21:54:49 +0200

    Zadanie 3 - tabela w bazie danych - poprawki

--- app/code/Bulbulatory/Recomendations/Setup/InstallSchema.php
+++ app/code/Bulbulatory/Recomendations/Setup/InstallSchema.php
@@ -20,71 +20,83 @@ class InstallSchema implements InstallSchemaInterface
      */
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        /**
-         * Create table 'recomendations'
-         */
-        $table = $setup->getConnection()
-                       ->newTable($setup->getTable('recomendations'))
-                       ->addColumn(
-                           'recomendation_id',
-                           Table::TYPE_INTEGER,
-                           null,
-                           ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
-                           'Recomendation Id'
-                       )
-                       ->addColumn(
-                           'customer_id',
-                           Table::TYPE_INTEGER,
-                           255,
-                           ['nullable' => false, 'default' => '', 'unsigned' => true],
-                           'Reference Id'
-                       )
-                       ->addColumn(
-                           'email',
-                           Table::TYPE_TEXT,
-                           100,
-                           ['nullable' => false],
-                           'Email to which was recomendation sent',
-                       )
-                       ->addColumn(
-                           'recomendation_hash',
-                           Table::TYPE_TEXT,
-                           255,
-                           ['nullable' => false],
-                           'Unique hash of recomendation',
-                       )
-                       ->addColumn(
-                           'status',
-                           Table::TYPE_BOOLEAN,
-                           null,
-                           ['identity' => true, 'nullable' => false],
-                           'Recomendation status'
-                       )
-                       ->addColumn(
-                           'created_at',
-                           Table::TYPE_TIMESTAMP,
-                           null,
-                           ['nullable' => false, 'default' => Table::TIMESTAMP_INIT],
-                           'Creation time of recomendation'
-                       )
-                       ->addColumn(
-                           'updated_at',
-                           Table::TYPE_TIMESTAMP,
-                           null,
-                           ['nullable' => false, 'default' => Table::TIMESTAMP_UPDATE],
-                           'Update time of recomendation'
-                       )
-                        ->addIndex(
-                            $setup->getIdxName(
-                                'recomendations',
-                                ['recomendation_hash'],
-                                AdapterInterface::INDEX_TYPE_UNIQUE
-                            ),
-                            'recomendation_hash',
-                            ['type' => AdapterInterface::INDEX_TYPE_UNIQUE]
-                        )
-                       ->setComment("Recomendations Table");
-        $setup->getConnection()->createTable($table);
+        if (!$setup->tableExists('recomendations')) {
+            /**
+             * Create table 'recomendations'
+             */
+            $table = $setup->getConnection()
+                           ->newTable($setup->getTable('recomendations'))
+                           ->addColumn(
+                               'recomendation_id',
+                               Table::TYPE_INTEGER,
+                               null,
+                               ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+                               'Recomendation Id'
+                           )
+                           ->addColumn(
+                               'customer_id',
+                               Table::TYPE_INTEGER,
+                               255,
+                               ['nullable' => false, 'default' => '', 'unsigned' => true],
+                               'Reference Id'
+                           )
+                           ->addColumn(
+                               'email',
+                               Table::TYPE_TEXT,
+                               100,
+                               ['nullable' => false],
+                               'Email to which was recomendation sent',
+                           )
+                           ->addColumn(
+                               'recomendation_hash',
+                               Table::TYPE_TEXT,
+                               255,
+                               ['nullable' => false],
+                               'Unique hash of recomendation',
+                           )
+                           ->addColumn(
+                               'status',
+                               Table::TYPE_BOOLEAN,
+                               null,
+                               ['nullable' => false, 'default' => 0],
+                               'Recomendation status'
+                           )
+                           ->addColumn(
+                               'created_at',
+                               Table::TYPE_TIMESTAMP,
+                               null,
+                               ['nullable' => false, 'default' => Table::TIMESTAMP_INIT],
+                               'Creation time of recomendation'
+                           )
+                           ->addColumn(
+                               'updated_at',
+                               Table::TYPE_TIMESTAMP,
+                               null,
+                               ['nullable' => false],
+                               'Update time of recomendation'
+                           )
+                           ->addIndex(
+                               $setup->getIdxName(
+                                   'recomendations',
+                                   ['recomendation_hash'],
+                                   AdapterInterface::INDEX_TYPE_UNIQUE
+                               ),
+                               'recomendation_hash',
+                               ['type' => AdapterInterface::INDEX_TYPE_UNIQUE]
+                           )
+                           ->setComment("Recomendations Table")
+            ->addForeignKey(
+                $setup->getFkName(
+                    'recomendations',
+                    'customer_id',
+                    'customer_entity',
+                    'entity_id'),
+                'customer_id',
+                $setup->getTable('customer_entity'),
+                'entity_id'
+            );
+            $setup->getConnection()->createTable($table);
+        }
     }
 }
 
